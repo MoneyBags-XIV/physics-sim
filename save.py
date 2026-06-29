f0fb97fadc306976cd5b82a8870c9ab349737caa
Lenzini <isaac.lenzini@gmail.com>
2026-06-28 21:20:52 -0400

    adds basic save class

---
+++ save.py
@@ -0,0 +1,11 @@
+class Save:
+    def __init__(self, masses, springs, lines, paused, gravity, force, snap, walls, collisions):
+        self.masses = masses
+        self.springs = springs
+        self.lines = lines
+        self.paused = paused
+        self.gravity = gravity
+        self.force = force
+        self.snap = snap
+        self.walls = walls
+        self.collisions = collisions
